fdce93ccc7efd9f92c83786d909ecb81376448ef
Ankon <44019225+luciferankon@users.noreply.github.com>
2018-11-27 17:04:46 +0530

    generated valid neighbours

--- src/lib.js
+++ src/lib.js
@@ -19,7 +19,32 @@ let board = {
                     return arrangeCells(this.grid,process.argv);
                   }
 };
+
+let possibleCombinations = [[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]];
+
+const generatePossibleNeighbours = function(currPosition){
+  return function(list){
+    let rowIndex = currPosition[0]+list[0];
+    let columnIndex = currPosition[1]+list[1];
+    return [rowIndex,columnIndex];
+  }
+}
+
+const checkValidNeighbour = function(grid){
+  return function(position){
+    return grid[position[0]]!=undefined && grid[position[1]]!=undefined;
+  }
+}
+
+const validCombinations = function(grid,currPosition){
+   let possibleNeighbours = possibleCombinations.map(generatePossibleNeighbours(currPosition));
+  return possibleNeighbours.filter(checkValidNeighbour(grid));
+}
+
 module.exports = { 
   initCell,
-  arrangeCells
+  arrangeCells,
+  validCombinations,
+  checkValidNeighbour,
+  generatePossibleNeighbours
 };
--- test/libTest.js
+++ test/libTest.js
@@ -1,6 +1,9 @@
 const { 
   initCell,
-  arrangeCells
+  arrangeCells,
+  validCombinations,
+  checkValidNeighbour,
+  generatePossibleNeighbours
 } = require("../src/lib.js");
 const assert = require('assert');
 
@@ -13,5 +16,29 @@ describe("initCell",function(){
 describe("arrangeCells",function(){
   it("should return updated 3X3 array with inputs",function(){
     assert.deepEqual(arrangeCells(initCell(),[,,1,6,9]),[[1,0,0],[0,0,1],[0,0,1]]);
+  });
+});
+
+describe('valid combinations',function(){
+  it('should return valid neighbours for given position',function(){
+    assert.deepEqual(validCombinations(initCell(),[0,0]),[[0,1],[1,0],[1,1]]);
+  });
+});
+
+describe('check valid neighbour',function(){
+  let checkPosition = checkValidNeighbour(initCell());
+  it('should return true if the position valid',function(){
+    assert.deepEqual(checkPosition([0,0]),true);
+  });
+  it('should return false if the position is not valid',function(){
+    assert.deepEqual(checkPosition([0,-1]),false);
+    assert.deepEqual(checkPosition([-1,0]),false);
+  });
+});
+
+describe('generate possible neighbour',function(){
+  let calculateNeighbour = generatePossibleNeighbours([0,2]);
+  it('should return all the 8 possible neighbour of the given position',function(){
+    assert.deepEqual(calculateNeighbour([-1,-1]),[-1,1]);
   });
 });
